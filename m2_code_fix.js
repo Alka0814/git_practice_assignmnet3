6a806e97930362e93163e7004077f1a580bfe417
paulzpranay97 <paulpranay1997@gmail.com>
2022-12-10 22:18:08 +0530

    done

--- m2_code_fix.js
+++ m2_code_fix.js
@@ -1,11 +1,13 @@
+let num=0;
 let c=0;
 for(i=0;i<num;i++){
 if(num%i==0){
-count++;
+c++;
 }
 }
 if(c==2){
-console.log(i+"is prime);
-}else{
-console.log(i+"not prime"):
+console.log(i,"is prime);
+}
+else{
+console.log(i,"not prime");
 }
